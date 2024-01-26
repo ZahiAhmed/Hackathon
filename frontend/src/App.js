97bd810e0a000c0981a69dc33e53de324ddd5609
ZahiAhmed <zahi.ahmed33@gmail.com>
2024-01-26 15:12:15 -0500

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 function App() {
+  
   return (
-    <h1>Hello from App</h1>
+    <Switch>
+      <Route path="/home" />
+      <Route path="/about" />
+      <Route path="/donate" />
+      <Route exact path= "/"/>
+    </Switch>
   );
 }
 
