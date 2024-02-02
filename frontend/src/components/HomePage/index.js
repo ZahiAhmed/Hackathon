de631ea3d16f1429c41d160c0850dfd2d87ad883
meih15 <meihuang8328@gmail.com>
2024-02-01 23:53:49 -0500

    added console log to check for errors

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Carousel from "./Carousel";
 import Texts from "./Texts";
 import Stats from "./Stats";
 import Video from "./Video";
+import { useLanguage } from "../LanguageSwitcher/LanguageContext";
 import "./index.css";
 
 const HomePage = () => {
- 
+  const { selectedLanguage } = useLanguage();
+  
   const [textToSpeak, setTextToSpeak] = useState("");
   const [statToSpeak, setStatToSpeak] = useState("");
+  
+  const speechSynthesisRef = useRef(null); // Ref to hold the speech synthesis instance
 
+  useEffect(() => {
+    console.log("Language changed, stopping speech synthesis...");
+    const speech = speechSynthesisRef.current;
+    if (speech) {
+      console.log("Speech object exists in ref.");
+      speechSynthesis.cancel(); // Cancel the speech synthesis
+      console.log("Speech synthesis stopped.");
+    } else {
+      console.log("Speech object does not exist in ref.");
+    }
+  }, [selectedLanguage]);
 
   const handleSpeak = () => {
     if (textToSpeak) {
       const speech = new SpeechSynthesisUtterance(textToSpeak);
+      speech.lang = selectedLanguage === 'es' ? 'es-ES' : 'en-US';
+      speechSynthesisRef.current = speech; // Store the speech synthesis instance in the ref
       window.speechSynthesis.speak(speech);
     }
   };
 
   const handleStatsSpeak = () => {
-    
-    // Speak the stats text
     if (statToSpeak) {
       const speech = new SpeechSynthesisUtterance(statToSpeak);
+      speech.lang = selectedLanguage === 'es' ? 'es-ES' : 'en-US';
+      speechSynthesisRef.current = speech; // Store the speech synthesis instance in the ref
       window.speechSynthesis.speak(speech);
     }
   };
