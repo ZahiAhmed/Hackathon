120d7ba910ce9357c6bd63fb14037dfb3a555d76
meih15 <meihuang8328@gmail.com>
2024-02-02 21:06:41 -0500

    fixed play/stop audio functionality

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -7,6 +7,7 @@ import { useLanguage } from "../LanguageSwitcher/LanguageContext";
 import "./index.css";
 
 const HomePage = () => {
+  
   const { selectedLanguage } = useLanguage();
   
   const [textToSpeak, setTextToSpeak] = useState("");
